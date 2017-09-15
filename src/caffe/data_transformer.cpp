69df70035ed26115a5f2e280f95ea355308cefb5
Fang <fanfang@ebay.com>
2017-09-15 10:10:42 -0500

    adapted changes to bvlc-master ea455eb29393ebe6de9f14e88bfce9eae74edf6d

--- src/caffe/data_transformer.cpp
+++ src/caffe/data_transformer.cpp
@@ -257,7 +257,6 @@ void DataTransformer<Dtype>::Transform(const cv::Mat& cv_img,
   CHECK_GE(img_height, crop_size);
   CHECK_GE(img_width, crop_size);
 
-
   Dtype* mean = NULL;
   if (has_mean_file) {
     CHECK_EQ(img_channels, data_mean_.channels());
